27c10bf4c85aa2e2cffca8b30080c70a26a52d6e
Natasha Kelly <kellynatashav@yahoo.com>
2019-05-27 22:34:36 -0400

    done

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,27 @@
 
 // Complete this algo
 const minJumps = arr => {
+  let count = 0
 
-};
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > arr[0]){
+      count++
+      if(arr[i] > arr.length - i){
+        count++
+        break
+      }
+    }
+    else if(arr[0] > arr[i]){
+      if(arr[0] > arr.length - i){
+        count++
+        break
+      }
+    }
+    else if(arr[0] === arr.length - i){
+      count = i
+    }
+  }
+  return count
+}
 
 module.exports = minJumps
