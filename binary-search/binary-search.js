83341b077312f41afeec14c21d852e168111ac0b
Natasha Kelly <kellynatashav@yahoo.com>
2019-05-29 10:08:27 -0400

    completed BS extra credit

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -20,7 +20,6 @@ const binarySearch = (array, target) => {
 	} else if (target > midpointValue){
 		return binarySearch(array.slice(midpoint + 1), target)
 	} else {
-		console.log("false")
 		return false
 	}
 };
@@ -31,6 +30,43 @@ const binarySearch = (array, target) => {
 	Can you augment the function above to run in constant O(1) space?
 	This means, you cannot edit/copy the original array!
 	How can we use other pieces of data, like pointers, to avoid slicing?
+
+SOLUTION
+	function binarySearch(array, target) {
+		return binarySearchHelper(array, target, 0, array.length - 1)
+	}
+
+
+		function binarySearchHelper(array, target, left, right){
+
+
+	//  base case
+				if(left > right){
+					return false
+				}
+
+			let midpoint = Math.floor((left + right) / 2)
+
+	// second base case
+				if(array[midpoint] === target){
+					return true
+				}
+				else if(target > array[midpoint]){
+				// 		the left is no longer worth looking into
+				return binarySearchHelper(array, target, left = midpoint + 1, right)
+				}
+				else if(target < array[midpoint]){
+				// 		the right is no longer worth looking into
+				return binarySearchHelper(array, target, left, right = midpoint - 1)
+				}
+				else{
+					return false
+				}
+
+
+
+	}
+
 
 */
 
